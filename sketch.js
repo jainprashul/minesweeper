da0694eec6a6208c4e04e745483fac25fed43215
jainprashul <jainprashul@gmail.com>
2018-04-09 21:09:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 function make2dArray(rows, cols) {
+    // Make the 2d Array
     var arr = new Array(cols);
     for(var i = 0 ; i< arr.length ; i++ ){
         arr[i] = new Array(rows);
@@ -7,19 +8,33 @@ function make2dArray(rows, cols) {
 }
 
 var grid;
-var w = 40;
+var w = 40; //width size
 var cols ;
 var rows ;
 var totalBees = 20;
-
+var button;
+var options = [];
+var choice = [];
+var canvas;
+var button2;
+var hints = 3;
 
 function setup() {
 
-    createCanvas(401,401);
+    canvas = createCanvas(401,401);
+    canvas.position(width/2 - 200,100);
     cols = floor(width/w);
     rows = floor(height/w);
     grid = make2dArray(rows, cols);
+    button = createButton('Reload');
+    button.mousePressed(reloadGame);
+    button.position(width/2 -60, 550);
+    button2 = createButton('Hint');
+    button2.mousePressed(hint);
+    button2.position(width/2 + 60, 550);
 
+
+    // create the new cells
     for(var i = 0 ; i< grid.length ; i++ ) {
         for(var j = 0 ; j< grid.length ; j++ ) {
                 grid[i][j] = new Cell(i ,j ,w);
@@ -27,21 +42,22 @@ function setup() {
         }
 
     // pick bees spots
-    var options = [];
-    for(var i = 0 ; i< rows ; i++ ) {
-        for(var j = 0 ; j< cols ; j++ ) {
-            options.push([i,j]);
+    {
+        for(var i = 0 ; i< rows ; i++ ) {
+            for(var j = 0 ; j< cols ; j++ ) {
+                options.push([i,j]);
+            }
         }
-    }
 
-    for (var n = 0 ; n < totalBees ; n++){
-        var index = floor(random(options.length));
-        var choice = options[index];
-        console.log(choice);
-        var x = choice[0];
-        var y = choice[1];
-        options.slice(index,1);
-        grid[x][y].bee = true;
+        for (var n = 0 ; n < totalBees ; n++){
+            var select = random(options);
+            choice.push(select);
+            var x = select[0];
+            var y = select[1];
+
+            grid[x][y].bee = true;
+        }
+
     }
 
 
@@ -72,6 +88,7 @@ function gameOver() {
     for(var i = 0 ; i< rows ; i++ ) {
         for(var j = 0 ; j< cols ; j++ ) {
             grid[i][j].reveal();
+            setTimeout(reloadGame , 2000);
         }
     }
 }
@@ -85,3 +102,24 @@ function draw() {
     }
 }
 
+function reloadGame() {
+    document.location.reload();
+}
+
+
+function hint() {
+    if (hints == 0){
+        return(alert("Exceed Number of hints"));
+    }
+    else {
+        hints--;
+        alert(hints + " hints Left.");
+    }
+    var index = floor(random(choice.length));
+    var hintCell = choice[index];
+    choice.splice(index , 1);
+    let x = hintCell[0];
+    let y = hintCell[1];
+    grid[x][y].revealed = true;
+
+}
